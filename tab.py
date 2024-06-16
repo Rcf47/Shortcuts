e86c44c24f904f07ec56720c28283aa97435aebf
Vadim Cherepanov <xenos26@gmail.com>
2024-06-16 17:41:45 +0700

    feat: add tab script for google chrome initial commit

---
+++ tab.py
@@ -0,0 +1,14 @@
+# Enter script code
+import time
+
+keyboard.send_keys("<ctrl>+l")
+time.sleep(0.1)
+keyboard.send_keys("<ctrl>+c")
+time.sleep(0.1)
+keyboard.send_keys("<ctrl>+w")
+time.sleep(0.1)
+keyboard.send_keys("<ctrl>+n")
+time.sleep(0.1)
+keyboard.send_keys("<ctrl>+v")
+time.sleep(0.1)
+keyboard.send_keys("<enter>")
